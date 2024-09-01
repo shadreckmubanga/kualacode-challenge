af947d4f63e7bbe5682e77be647a353f7e86b5d4
shadreckmubanga <145011409+shadreckmubanga@users.noreply.github.com>
2024-09-01 14:04:42 +0200

    Add files via upload
    Kuala Code challenge files

---
+++ script.js
@@ -0,0 +1,84 @@
+document.getElementById('hr').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "hr1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/10yVMCTFVoxPe_MLiZs1CQkojseJKf4VA/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
+
+
+document.getElementById('finance').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "finance1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/10zEh_w6z4iDeyfCsK7NQrRPDJhvGSEqj/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
+
+document.getElementById('marketing').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "marketing1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/1XdVWwF_fhpIfB7Tk9ZO7cZY-E937sn4d/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
+
+document.getElementById('admin').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "admin1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/1124Hl8_4c3uHXc-XHfAjLLeobWSNkkqV/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
+
+document.getElementById('cs').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "cs1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/117hB5QktzklmkElCXZ5FRg09suIWFDCn/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
+
+document.getElementById('it').addEventListener('click', function() {
+    let password = prompt("Enter password");
+
+    let correctpassword = "it1234";
+
+    if(password === correctpassword) {
+        let link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/1MgPegl6Ka_US1JEIvBqBw4ITEf2xpQM7/view?usp=drivesdk';
+        link.click();
+    } else {
+        alert("Incorrect password");
+    }
+});
